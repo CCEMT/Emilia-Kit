f3efe91179e6c0428a77bb4aa4de6f6ffca41fe2
EMT <1076995595@qq.com>
2025-01-06 17:01:18 +0800

    调整反射TimeArea

--- Reflection/TimeArea_Internals.cs
+++ Reflection/TimeArea_Internals.cs
@@ -4,8 +4,25 @@ namespace Emilia.Reflection.Editor
 {
     public class TimeArea_Internals : TimeArea
     {
+        public enum TimeFormat_Internals
+        {
+            None,
+            TimeFrame,
+            Frame,
+        }
+
         public TimeArea_Internals(bool minimalGUI) : base(minimalGUI) { }
         public TimeArea_Internals(bool minimalGUI, bool enableSliderZoom) : base(minimalGUI, enableSliderZoom) { }
         public TimeArea_Internals(bool minimalGUI, bool enableSliderZoomHorizontal, bool enableSliderZoomVertical) : base(minimalGUI, enableSliderZoomHorizontal, enableSliderZoomVertical) { }
+
+        public override string FormatTickTime(float time, float frameRate, TimeFormat timeFormat)
+        {
+            return FormatTickTime_Internals(time, frameRate, (TimeFormat_Internals) timeFormat);
+        }
+
+        public virtual string FormatTickTime_Internals(float time, float frameRate, TimeFormat_Internals timeFormat)
+        {
+            return base.FormatTickTime(time, frameRate, (TimeFormat) timeFormat);
+        }
     }
 }
