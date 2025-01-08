a088a408859dd55efc414cf3e3fd88c6e87af940
EMT <1076995595@qq.com>
2025-01-08 14:41:49 +0800

    调整反射TreeView

---
+++ Reflection/TreeViewDragging_Internal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+
+namespace Emilia.Reflection.Editor
+{
+    public class TreeViewDragging_Internal : TreeViewDragging
+    {
+        public TreeViewDragging_Internal(TreeViewController treeView) : base(treeView) { }
+
+        public override void StartDrag(TreeViewItem draggedItem, List<int> draggedItemIDs)
+        {
+            StartDrag_Internal(draggedItem, draggedItemIDs);
+        }
+
+        public virtual void StartDrag_Internal(TreeViewItem draggedItem, List<int> draggedItemIDs) { }
+
+        public override DragAndDropVisualMode DoDrag(TreeViewItem parentItem, TreeViewItem targetItem, bool perform, DropPosition dropPosition)
+        {
+            return DoDrag_Internal(parentItem, targetItem, perform, dropPosition);
+        }
+
+        public virtual DragAndDropVisualMode DoDrag_Internal(TreeViewItem parentItem, TreeViewItem targetItem, bool perform, DropPosition dropPosition)
+        {
+            return default;
+        }
+    }
+}
