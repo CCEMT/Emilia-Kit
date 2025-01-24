0184608cab50969aa28913cdff178011d1be8d46
EMT <1076995595@qq.com>
2025-01-24 16:02:15 +0800

    调整反射TreeView

--- Reflection/TreeView/ITreeViewDragging_Internal.cs
+++ Reflection/TreeView/ITreeViewDragging_Internal.cs
@@ -1,6 +1,24 @@
-﻿using UnityEditor.IMGUI.Controls;
+﻿using System.Collections.Generic;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
 
 namespace Emilia.Reflection.Editor
 {
-    public interface ITreeViewDragging_Internal : ITreeViewDragging { }
+    public interface ITreeViewDragging_Internal : ITreeViewDragging
+    {
+        bool drawRowMarkerAbove_Internal { get; set; }
+        void OnInitialize_Internal();
+
+        bool CanStartDrag_Internal(TreeViewItem targetItem, List<int> draggedItemIDs, Vector2 mouseDownPosition);
+
+        void StartDrag_Internal(TreeViewItem draggedItem, List<int> draggedItemIDs);
+
+        bool DragElement_Internal(TreeViewItem targetItem, Rect targetItemRect, int row);
+
+        void DragCleanup_Internal(bool revertExpanded);
+
+        int GetDropTargetControlID_Internal();
+
+        int GetRowMarkerControlID_Internal();
+    }
 }
