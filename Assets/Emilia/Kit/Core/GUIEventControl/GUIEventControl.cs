fa4076332ec82aa42129e832d22237ad85206f67
EMT <1076995595@qq.com>
2025-02-08 16:16:40 +0800

    增加GUI事件管理

---
+++ Assets/Emilia/Kit/Core/GUIEventControl/GUIEventControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Emilia.Kit
+{
+    public class GUIEventControl
+    {
+        private readonly List<GUIEventManipulator> _manipulators;
+
+        public GUIEventControl(params GUIEventManipulator[] manipulators)
+        {
+            this._manipulators = new List<GUIEventManipulator>(manipulators);
+        }
+
+        public GUIEventControl(IEnumerable<GUIEventManipulator> manipulators)
+        {
+            this._manipulators = new List<GUIEventManipulator>(manipulators);
+        }
+
+        public bool HandleManipulatorsEvents(object userData)
+        {
+            bool isHandled = false;
+
+            for (var i = 0; i < this._manipulators.Count; i++)
+            {
+                GUIEventManipulator manipulator = this._manipulators[i];
+                isHandled = manipulator.HandleEvent(userData);
+                if (isHandled) break;
+            }
+
+            return isHandled;
+        }
+    }
+}
