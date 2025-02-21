864c4a77056d8f9346afaa8e8bbdea1cfe3d4b2e
EMT <1076995595@qq.com>
2025-02-21 11:54:18 +0800

    增加EditorGUIUtility

---
+++ Reflection/GUI/EditorGUIUtility_Internal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Emilia.Reflection.Editor
+{
+    public static class EditorGUIUtility_Internal
+    {
+        public static Texture2D LoadIcon_ByName(string name)
+        {
+            return EditorGUIUtility.LoadIcon(name);
+        }
+    }
+}
