1985f93de6d32dfe66b422e19520312a94d106ec
EMT <1076995595@qq.com>
2025-03-13 16:21:02 +0800

    反射调整

---
+++ Reflection/GUI/EditorGUILayout_Internals.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Emilia.Reflection.Editor
+{
+    public static class EditorGUILayout_Internals
+    {
+        public static Rect LastRect_Internals => EditorGUILayout.s_LastRect;
+
+        public static Rect BeginHorizontal_Internals(GUIContent content, GUIStyle style, params GUILayoutOption[] options)
+        {
+            return EditorGUILayout.BeginHorizontal(content, style, options);
+        }
+
+        public static Rect BeginVertical_Internals(GUIContent content, GUIStyle style, params GUILayoutOption[] options)
+        {
+            return EditorGUILayout.BeginVertical(content, style, options);
+        }
+
+        public static Vector2 BeginScrollView_Internals(Vector2 scrollPosition, bool alwaysShowHorizontal, bool alwaysShowVertical, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar,
+            params GUILayoutOption[] options)
+        {
+            return EditorGUILayout.BeginScrollView(scrollPosition, alwaysShowHorizontal, alwaysShowVertical, horizontalScrollbar, verticalScrollbar, options);
+        }
+    }
+}
