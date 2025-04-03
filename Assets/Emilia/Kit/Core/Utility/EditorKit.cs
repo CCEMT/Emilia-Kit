83415ab966b82c20c9a42f59b162f3e46b8f3bdf
EMT <1076995595@qq.com>
2025-04-03 12:09:49 +0800

    增加一些实用函数

--- Assets/Emilia/Kit/Core/Utility/EditorKit.cs
+++ Assets/Emilia/Kit/Core/Utility/EditorKit.cs
@@ -1,9 +1,11 @@
 ﻿#if UNITY_EDITOR
 using System;
+using System.Text.RegularExpressions;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Object = UnityEngine.Object;
 
 namespace Emilia.Kit
@@ -29,13 +31,6 @@ namespace Emilia.Kit
             }
         }
 
-        public static void Save(this Object target, bool isRefresh = false)
-        {
-            EditorUtility.SetDirty(target);
-            AssetDatabase.SaveAssets();
-            if (isRefresh) AssetDatabase.Refresh();
-        }
-
         public static void SetSelection(object target, string disposeName = null)
         {
             if (string.IsNullOrEmpty(disposeName)) disposeName = target.ToString();
@@ -43,6 +38,33 @@ namespace Emilia.Kit
             selectionContainer.target = target;
             selectionContainer.displayName = disposeName;
             Selection.activeObject = selectionContainer;
+        }
+
+        public static string GetNumberAlpha(string source)
+        {
+            if (string.IsNullOrEmpty(source)) return null;
+            string pattern = "[A-Za-z0-9_]";
+            string strRet = "";
+            MatchCollection results = Regex.Matches(source, pattern);
+            foreach (Match v in results) strRet += v.ToString();
+            return strRet;
+        }
+
+        public static string GetWholePath(Transform currentGameObject, GameObject target)
+        {
+            if (currentGameObject.parent == null || currentGameObject.parent.gameObject == target) return currentGameObject.name;
+            return GetWholePath(currentGameObject.parent, target) + "/" + currentGameObject.name;
+        }
+
+        public static void ClearScene(Scene scene)
+        {
+            GameObject[] previewGameObjects = scene.GetRootGameObjects();
+            int previewAmount = previewGameObjects.Length;
+            for (int i = 0; i < previewAmount; i++)
+            {
+                GameObject previewGameObject = previewGameObjects[i];
+                Object.DestroyImmediate(previewGameObject);
+            }
         }
 
         [HideMonoScript]
---
+++ Reflection/SceneHierarchyWindow_Internals.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace Emilia.Reflection.Editor
+{
+    public class SceneHierarchyWindow_Internals : SceneHierarchyWindow
+    {
+        public static Type sceneHierarchyWindowType_Internals => typeof(SceneHierarchyWindow);
+
+        public static EditorWindow lastInteractedHierarchyWindow_Internals => lastInteractedHierarchyWindow;
+
+        public static List<EditorWindow> GetAllSceneHierarchyWindows_Internals() => GetAllSceneHierarchyWindows().OfType<EditorWindow>().ToList();
+
+        public void ReloadData_Internals()
+        {
+            ReloadData();
+        }
+
+        public static void RebuildStageHeaderInAll_Internals()
+        {
+            RebuildStageHeaderInAll();
+        }
+
+        public static void ReloadAllHierarchyWindows_Internals()
+        {
+            ReloadAllHierarchyWindows();
+        }
+
+        public static EditorWindow GetSceneHierarchyWindowToFocusForNewGameObjects_Internals() => GetSceneHierarchyWindowToFocusForNewGameObjects();
+    }
+}
