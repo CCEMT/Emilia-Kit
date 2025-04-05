f32802bd52224a00fa71509aa163754486c942b8
EMT <1076995595@qq.com>
2025-04-05 13:18:43 +0800

    选中处理

---
+++ Assets/Emilia/Kit/Core/Selected/ISelectedHandle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Emilia.Kit
+{
+    public interface ISelectedHandle
+    {
+        bool Validate();
+
+        bool IsSelected();
+
+        void Select();
+
+        void Unselect();
+
+        IEnumerable<Object> GetSelectedObjects();
+    }
+}
