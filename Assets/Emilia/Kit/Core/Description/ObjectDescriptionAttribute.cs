3c329fe980b410577d2b99ff9522351d0f3d333c
EMT <1076995595@qq.com>
2025-04-05 19:59:31 +0800

    描述处理

---
+++ Assets/Emilia/Kit/Core/Description/ObjectDescriptionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Emilia.Kit
+{
+    public class ObjectDescriptionAttribute : Attribute
+    {
+        public Type objectType;
+
+        public ObjectDescriptionAttribute(Type objectType)
+        {
+            this.objectType = objectType;
+        }
+    }
+}
