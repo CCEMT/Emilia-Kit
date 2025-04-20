772bc5c017e63abf156ab85621bf3daadb42fb12
EMT <1076995595@qq.com>
2025-04-20 17:36:12 +0800

    反射增加GUILayoutUtility_Internals

---
+++ Reflection/GUI/GUILayoutUtility_Internals.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Emilia.Reflection.Editor
+{
+    public static class GUILayoutUtility_Internals
+    {
+        public static Rect PeekNext_Internals() => GUILayoutUtility.current.topLevel.PeekNext();
+    }
+}
