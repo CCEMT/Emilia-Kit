21eeed7ec939d17969263b20d9a2df084cfb5aef
EMT <1076995595@qq.com>
2025-04-26 18:29:47 +0800

    增加一些函数

--- Reflection/GUI/GUILayoutUtility_Internals.cs
+++ Reflection/GUI/GUILayoutUtility_Internals.cs
@@ -5,5 +5,9 @@ namespace Emilia.Reflection.Editor
     public static class GUILayoutUtility_Internals
     {
         public static Rect PeekNext_Internals() => GUILayoutUtility.current.topLevel.PeekNext();
+
+        public static int GetTopLevelEntryCount_Internals() => GUILayoutUtility.current.topLevel.entries.Count;
+
+        public static int GetTopLevelCurrentCount_Internals() => GUILayoutUtility.current.topLevel.m_Cursor;
     }
 }
