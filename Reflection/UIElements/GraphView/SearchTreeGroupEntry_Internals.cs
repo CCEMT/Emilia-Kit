a01f3b7d2732ffceffee11d86050f4497d412476
EMT <1076995595@qq.com>
2025-05-25 14:15:07 +0800

    SearchWindow_Hook实现

---
+++ Reflection/UIElements/GraphView/SearchTreeGroupEntry_Internals.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace Emilia.Reflection.Editor
+{
+    public static class SearchTreeGroupEntry_Internals
+    {
+        public static int GetSelectedIndex(this SearchTreeGroupEntry groupEntry) => groupEntry.selectedIndex;
+        
+        public static void SetSelectedIndex(this SearchTreeGroupEntry groupEntry, int index) => groupEntry.selectedIndex = index;
+
+        public static Vector2 GetScrollPosition(this SearchTreeGroupEntry groupEntry) => groupEntry.scroll;
+        
+        public static void SetScrollPosition(this SearchTreeGroupEntry groupEntry, Vector2 scroll) => groupEntry.scroll = scroll;
+    }
+}
