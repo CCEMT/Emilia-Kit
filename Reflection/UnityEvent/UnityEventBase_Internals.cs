60ed3435f86d4566b332edc1468f0ce27c51c44e
EMT <1076995595@qq.com>
2025-08-07 14:21:32 +0800

    增加内部UnityEvent拓展

---
+++ Reflection/UnityEvent/UnityEventBase_Internals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine.Events;
+using Object = UnityEngine.Object;
+
+namespace Emilia.Reflection.Editor
+{
+    public static class UnityEventBase_Extensions
+    {
+        public static void RegisterVoidPersistentListenerWithoutValidation_Internals(
+            this UnityEventBase thisUnityEventBase,
+            int index,
+            Object target,
+            Type targetType,
+            string methodName)
+        {
+            thisUnityEventBase.RegisterVoidPersistentListenerWithoutValidation(index, target, targetType, methodName);
+        }
+    }
+}
---
+++ Reflection/UnityEvent/UnityEvent_Internals.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.Events;
+
+namespace Emilia.Reflection.Editor
+{
+    public static class UnityEvent_Extensions
+    {
+        public static void AddPersistentListener_Internals(this UnityEngine.Events.UnityEvent thisUnityEventBase, UnityAction call)
+        {
+            thisUnityEventBase.AddPersistentListener(call);
+        }
+
+        public static void AddPersistentListener_Internals<T0>(this UnityEvent<T0> thisUnityEventBase, UnityAction<T0> call)
+        {
+            thisUnityEventBase.AddPersistentListener(call);
+        }
+
+        public static void AddPersistentListener_Internals<T0, T1>(this UnityEvent<T0, T1> thisUnityEventBase, UnityAction<T0, T1> call)
+        {
+            thisUnityEventBase.AddPersistentListener(call);
+        }
+
+        public static void AddPersistentListener_Internals<T0, T1, T2>(this UnityEvent<T0, T1, T2> thisUnityEventBase, UnityAction<T0, T1, T2> call)
+        {
+            thisUnityEventBase.AddPersistentListener(call);
+        }
+
+        public static void AddPersistentListener_Internals<T0, T1, T2, T3>(this UnityEvent<T0, T1, T2, T3> thisUnityEventBase, UnityAction<T0, T1, T2, T3> call)
+        {
+            thisUnityEventBase.AddPersistentListener(call);
+        }
+    }
+}
