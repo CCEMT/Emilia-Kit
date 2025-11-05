745271ba31b7f1feaeb859809029e644dcbf5763
EMT <1076995595@qq.com>
2025-11-05 17:36:34 +0800

    增加Edge内部类

--- Reflection/UIElements/GraphView/Edge_Internals.cs
+++ Reflection/UIElements/GraphView/Edge_Internals.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Emilia.Reflection.Editor
 {
@@ -8,5 +10,32 @@ namespace Emilia.Reflection.Editor
         {
             edge.ForceUpdateEdgeControl();
         }
+    }
+
+    public class Edge_Internals : Edge
+    {
+        public void UpdateEdgeControlColorsAndWidth_Internal() => UpdateEdgeControlColorsAndWidth();
+
+        public Vector2 GetPortPosition_Internal(Port p) => GetPortPosition(p);
+
+        public void TrackGraphElement_Internal(Port port) => TrackGraphElement(port);
+
+        public void OnPortDetach_Internal(DetachFromPanelEvent e) => OnPortDetach(e);
+
+        public void OnPortAttach_Internal(AttachToPanelEvent e) => OnPortAttach(e);
+
+        public void OnEdgeAttach_Internal(AttachToPanelEvent e) => OnEdgeAttach(e);
+
+        public void UntrackGraphElement_Internal(Port port) => UntrackGraphElement(port);
+
+        public void DoTrackGraphElement_Internal(Port port) => DoTrackGraphElement(port);
+
+        public void DoUntrackGraphElement_Internal(Port port) => DoUntrackGraphElement(port);
+
+        public void OnPortGeometryChanged_Internal(GeometryChangedEvent evt) => OnPortGeometryChanged(evt);
+
+        public void OnGeometryChanged_Internal(GeometryChangedEvent evt) => OnGeometryChanged(evt);
+
+        public static bool Approximately_Internal(Vector2 v1, Vector2 v2) => Approximately(v1, v2);
     }
 }
