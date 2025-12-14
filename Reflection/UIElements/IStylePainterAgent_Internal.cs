cacc2ff81e88d74f25c24475882b761b98ae6627
EMT <1076995595@qq.com>
2025-12-14 15:05:28 +0800

    反射增加MeshGenerationContext相关

---
+++ Reflection/UIElements/IStylePainterAgent_Internal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Emilia.Reflection.Editor
+{
+    public struct IStylePainterAgent_Internal
+    {
+        private IStylePainter stylePainter;
+
+        public IStylePainterAgent_Internal(IStylePainter stylePainter)
+        {
+            this.stylePainter = stylePainter;
+        }
+
+        public VisualElement visualElement => this.stylePainter.visualElement;
+
+        public MeshWriteData DrawMesh_Internal(
+            int vertexCount,
+            int indexCount,
+            Texture texture,
+            Material material,
+            MeshGenerationContext_Internal.MeshFlags_Internals flags)
+            => this.stylePainter.DrawMesh(vertexCount, indexCount, texture, material, (MeshGenerationContext.MeshFlags) flags);
+
+        public void DrawText(
+            MeshGenerationContextUtils.TextParams textParams,
+            ITextHandle handle,
+            float pixelsPerPoint)
+            => this.stylePainter.DrawText(textParams, handle, pixelsPerPoint);
+
+        public void DrawRectangle(MeshGenerationContextUtils.RectangleParams rectParams) => this.stylePainter.DrawRectangle(rectParams);
+
+        public void DrawBorder(MeshGenerationContextUtils.BorderParams borderParams) => this.stylePainter.DrawBorder(borderParams);
+
+        public void DrawImmediate(Action callback, bool cullingEnabled) => this.stylePainter.DrawImmediate(callback, cullingEnabled);
+    }
+}
---
+++ Reflection/UIElements/MeshWriteData_Internal.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Emilia.Reflection.Editor
+{
+    public static class MeshWriteData_Internal
+    {
+        public static NativeSlice<Vertex> GetVertices_Internal(this MeshWriteData self) => self.m_Vertices;
+        public static void SetVertices_Internal(this MeshWriteData self, NativeSlice<Vertex> vertices) => self.m_Vertices = vertices;
+
+        public static NativeSlice<ushort> GetIndices_Internal(this MeshWriteData self) => self.m_Indices;
+        public static void SetIndices_Internal(this MeshWriteData self, NativeSlice<ushort> indices) => self.m_Indices = indices;
+
+        public static Rect GetUVRegion_Internal(this MeshWriteData self) => self.m_UVRegion;
+        public static void SetUVRegion_Internal(this MeshWriteData self, Rect uvRegion) => self.m_UVRegion = uvRegion;
+
+        public static int GetCurrentIndex_Internal(this MeshWriteData self) => self.currentIndex;
+        public static void SetCurrentIndex_Internal(this MeshWriteData self, int currentIndex) => self.currentIndex = currentIndex;
+
+        public static int GetCurrentVertex_Internal(this MeshWriteData self) => self.currentVertex;
+        public static void SetCurrentVertex_Internal(this MeshWriteData self, int currentVertex) => self.currentVertex = currentVertex;
+
+        public static void Reset(this MeshWriteData self, NativeSlice<Vertex> vertices, NativeSlice<ushort> indices) => self.Reset(vertices, indices);
+        public static void Reset(this MeshWriteData self, NativeSlice<Vertex> vertices, NativeSlice<ushort> indices, Rect uvRegion) => self.Reset(vertices, indices, uvRegion);
+    }
+}
